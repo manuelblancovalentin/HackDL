92b7653528675826296a265e87a992cd51b8f557
Manu Blanco Valentín <manuelblancovalentin@users.noreply.github.com>
2021-04-16 12:11:35 -0500

    Updated travis setup

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -6,7 +6,7 @@ export CMAKE_VERSION=3.20.1
 wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz
 tar -zxf cmake-$CMAKE_VERSION.tar.gz
 cd cmake-$CMAKE_VERSION
-sudo ./bootsrap
+sudo ./bootstrap
 make
 make install
 cmake --version
