450b5f151cb2bb66869e155cf3abe9c31b6186d1
Manu Blanco Valentín <manuelblancovalentin@users.noreply.github.com>
2021-04-16 11:59:40 -0500

    Updated travis setup

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -2,5 +2,14 @@
 
 sudo apt-get update
 # Install cmake
-sudo snap install cmake --classic
+export CMAKE_VERSION=3.20.1
+wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz
+tar -zxvf cmake-$CMAKE_VERSION.tar.gz
+cd cd cmake-$CMAKE_VERSION
+sudo ./bootsrap
+make
+make install
+cmake --version
+cd ..
+# install other dependencies
 sudo apt-get install -y libc++-dev libc++abi-dev bsdtar libedit-dev
