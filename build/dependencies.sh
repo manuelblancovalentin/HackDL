dabb04164c98f9a459aa9f56fdffdab6f41cfa84
Manu Blanco Valentín <manuelblancovalentin@users.noreply.github.com>
2021-04-16 12:25:12 -0500

    Updated travis setup

--- build/dependencies.sh
+++ build/dependencies.sh
@@ -3,8 +3,10 @@
 sudo apt-get update
 # Install gcc and other compilers
 sudo apt-get install build-essential
+sudo apt install gcc-8 g++-8 gcc-9 g++-9 gcc-10 g++-10
 # Install make
 sudo apt install make
+sudo apt install cmake
 # Install cmake
 export CMAKE_VERSION=3.20.1
 wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz
